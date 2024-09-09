4f34b7626c24e58991c56ec26447c72cc18333a7
Valdir Github <valdyrtorres@yahoo.com.br>
2024-09-08 22:08:02 -0300

    links

--- flash/website/urls.py
+++ flash/website/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('add.html', views.add, name="add"),
+    path('subtract.html', views.subtract, name="subtract"),
+    path('multiply.html', views.multiply, name="multiply"),
+    path('divide.html', views.divide, name="divide"),
 ]
--- flash/website/views.py
+++ flash/website/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
+
+def add(request):
+    return render(request, 'add.html', {})
+
+def subtract(request):
+    return render(request, 'subtract.html', {})
+
+def multiply(request):
+    return render(request, 'multiply.html', {})
+
+def divide(request):
+    return render(request, 'divide.html', {})
